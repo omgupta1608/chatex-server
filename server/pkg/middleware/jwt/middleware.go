ea58110c5905adc369cfbb73b1644393f7b3d7ce
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2021-07-04 23:54:56 +0530

    create a jwt auth middleware

---
+++ server/pkg/middleware/jwt/middleware.go
@@ -0,0 +1,42 @@
+package jwt
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// sends an unauthorized http response
+func sendUnauthorized(c *gin.Context, message string, err error) {
+	c.JSON(http.StatusUnauthorized, gin.H{
+		"message": message,
+		"error":   err,
+	})
+	c.Abort()
+}
+
+// middleware to authenticate using jwt token
+func JWTAuthMiddleware() gin.HandlerFunc {
+	const BEARER_SCHEMA = "Bearer"
+
+	return func(c *gin.Context) {
+		// get jwt stored in cookie
+		authHeader := c.GetHeader("Authorization")
+		if len(authHeader) <= len(BEARER_SCHEMA) {
+			sendUnauthorized(c, "Please login again. Bad Token", nil)
+			return
+		}
+		tokenString := authHeader[len(BEARER_SCHEMA)+1:]
+
+		// parse jwt token
+		user, err := ParseToken(tokenString)
+		if err != nil {
+			sendUnauthorized(c, "Please Login again. Bad Token", err)
+			return
+		}
+
+		// store user data in this context
+		c.Set("User", user)
+		c.Next()
+	}
+}
