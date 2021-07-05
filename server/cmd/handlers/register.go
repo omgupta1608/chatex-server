65cde9a2925def9d2e9d7cbfe3b9489b1443d6d8
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2021-07-05 19:53:47 +0530

    basic registeration route handler

---
+++ server/cmd/handlers/register.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"errors"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/omgupta1608/chatex/server/pkg/exception"
+	"github.com/omgupta1608/chatex/server/pkg/types"
+	"github.com/rs/xid"
+)
+
+// ! not for production
+var UserList []types.User
+
+func RegisterRouteHandler(c *gin.Context) {
+	// user data from request body
+	var user types.User
+
+	err := c.BindJSON(&user)
+	if err != nil {
+		exception.SendError(c, http.StatusBadRequest, errors.New("Bad JSON format"))
+		return
+	}
+
+	// TODO: sanitize client input
+	// TODO: validate client input
+
+	// initialize/generate user data
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		exception.SendError(c, http.StatusInternalServerError, errors.New("Could not hash the password"))
+		return
+	}
+	user.Password = string(hashedPassword)
+	user.Uid = xid.New().String()
+	if user.About == "" {
+		user.About = "Hello there!! I'm using ChatX"
+	}
+	user.Verified = false
+	// TODO: add a default profile Pic
+
+	// TODO: store this user in redis and wait for verification
+	UserList = append(UserList, user)
+
+	// TODO: create a verification token & send through email
+	// TODO: store verify token in redis
+
+	c.JSON(http.StatusCreated, gin.H{
+		"data":               user.Uid,
+		"message":            "User created",
+		"error":              nil,
+		"verification-route": c.FullPath() + "/verify",
+	})
+}
--- server/go.mod
+++ server/go.mod
@@ -15,9 +15,10 @@ require (
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/rs/xid v1.3.0
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
-	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
